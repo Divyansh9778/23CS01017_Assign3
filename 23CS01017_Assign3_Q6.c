6fcc6f5c045a868971365d1f8bfa1ae15feff104
Divyansh Sharma <143500382+Divyansh9778@users.noreply.github.com>
2024-01-31 03:08:23 +0530

    Update 23CS01017_Assign3_Q6.c

--- 23CS01017_Assign3_Q6.c
+++ 23CS01017_Assign3_Q6.c
@@ -2,7 +2,7 @@
 #include <math.h>
 int main()
 {
-    int n, len = 0, temp1, temp2;
+    int n, len = 0, temp1, temp2, i = 0;
     printf("Enter the number: ");
     scanf("%d", &n);
     int num = n * n;
@@ -14,18 +14,41 @@ int main()
         num1 = num1 / 10;
         len++;
     }
-
+    int len1 = len;
     len = (len / 2) + (len % 2);
-    double div = pow(10, len);
-    temp1 = num / div;
-    temp2 = num - temp1 * div;
 
-    if (temp1 + temp2 == n)
+    for (; len > 0; len--)
     {
-        printf("\nAs %d + %d = %d", temp1, temp2, n);
-        printf("\n%d is a KAPREKAR NUMBER", n);
+        double div = pow(10, len);
+        temp1 = num / div;
+        temp2 = num - temp1 * div;
+
+        if (temp1 + temp2 == n)
+        {
+            printf("\nAs %d + %d = %d", temp1, temp2, n);
+            printf("\n%d is a KAPREKAR NUMBER", n);
+            i++;
+            break;
+        }
     }
-    else
+    if (i == 0)
+    {
+        for (; len < len1; len++)
+        {
+            double div = pow(10, len);
+            temp1 = num / div;
+            temp2 = num - temp1 * div;
+
+            if (temp1 + temp2 == n)
+            {
+                printf("\nAs %d + %d = %d", temp1, temp2, n);
+                printf("\n%d is a KAPREKAR NUMBER", n);
+                i++;
+                break;
+            }
+        }
+    }
+    if (i == 0)
         printf("\n%d is NOT a KAPREKAR NUMBER", n);
     return 0;
 }
